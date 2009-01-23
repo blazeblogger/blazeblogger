8c9d879cf92c43d53bd27fd802b3a3d20b2d062f
Jaromir Hradilek <jhradilek@gmail.com>
2009-01-23 23:38:36 +0100

    Partly rewrited the post/page removing utility.
    Among minor, more or less cosmetic changes, the most important are:
    
      * the much complete set of command-line options with more obvious names
        and description;
      * making functions return values in order to recognise success.

--- blaze-remove.pl
+++ blaze-remove.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
-# blaze-remove, remove a blog post or a page from the Blaze repozitory
+# blaze-remove, remove a blog post or a page from the BlazeBlogger
+#               repository
 # Copyright (C) 2008, 2009 Jaromir Hradilek
 
 # This program is  free software:  you can redistribute it and/or modify it
@@ -28,14 +29,15 @@ use constant NAME    => basename($0, '.pl');        # Script name.
 use constant VERSION => '0.0.1';                    # Script version.
 
 # General script settings:
-our $type    = 'post';                              # Type: post or page.
-our $destdir = '.';                                 # Destination folder.
+our $blogdir = '.';                                 # Repository location.
 our $verbose = 1;                                   # Verbosity level.
+
+# Command-line options:
+my  $type    = 'post';                              # Type: post or page.
 
 # Set up the __WARN__ signal handler:
 $SIG{__WARN__} = sub {
-  my $message  = shift;
-  print STDERR NAME . ": $message";
+  print STDERR NAME . ": " . (shift);
 };
 
 # Display given message and terminate the script:
@@ -51,23 +53,30 @@ sub exit_with_error {
 sub display_help {
   my $NAME = NAME;
 
+  # Print the message to the STDOUT:
   print << "END_HELP";
-Usage: $NAME [-pqP] [-d directory] id
+Usage: $NAME [-pqPV] [-b directory] id
        $NAME -h | -v
 
-  -d, --destdir directory     specify the destination directory
+  -b, --blogdir directory     specify the directory where the BlazeBlogger
+                              repository is placed
   -p, --page                  remove the static page instead of the post
   -P, --post                  remove the blog post; the default option
   -q, --quiet                 avoid displaying unnecessary messages
+  -V, --verbose               display all messages; the default option
   -h, --help                  display this help and exit
   -v, --version               display version information and exit
 END_HELP
+
+  # Return success:
+  return 1;
 }
 
 # Display version information:
 sub display_version {
   my ($NAME, $VERSION) = (NAME, VERSION);
 
+  # Print the message to the STDOUT:
   print << "END_VERSION";
 $NAME $VERSION
 
@@ -77,27 +86,29 @@ distributed in the hope  that it will be useful,  but WITHOUT ANY WARRANTY;
 without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PAR-
 TICULAR PURPOSE.
 END_VERSION
+
+  # Return success:
+  return 1;
 }
 
 # Add given string to the log file
 sub add_to_log {
-  my $file = shift || die "Missing argument";
-  my $text = shift || '';
+  my $text = shift || 'Something miraculous has just happened!';
+  my $file = catfile($blogdir, '.blaze', 'log');
 
-  # Try to open the log file:
-  if (open(LOG, ">>$file")) {
-    # Write to the log file: 
-    print LOG "Date: ".strftime("%a %b %e %H:%M:%S %Y", localtime)."\n\n";
-    print LOG wrap('    ', '    ', $text);
-    print LOG "\n\n";
+  # Open the log file for appending:
+  open(LOG, ">>$file") or return 0;
 
-    # Close the file:
-    close(LOG);
-  }
-  else {
-    # Report failure and exit:
-    exit_with_error("Unable to write to `$file'.", 13);
-  }
+  # Write to the log file: 
+  print LOG "Date: ".strftime("%a %b %e %H:%M:%S %Y", localtime)."\n\n";
+  print LOG wrap('    ', '    ', $text);
+  print LOG "\n\n";
+
+  # Close the file:
+  close(LOG);
+
+  # Return success:
+  return 1;
 }
 
 # Set up the options parser:
@@ -110,36 +121,28 @@ GetOptions(
   'page|p'        => sub { $type    = 'page'; },
   'post|P'        => sub { $type    = 'post'; },
   'quiet|q'       => sub { $verbose = 0;      },
-  'destdir|d=s'   => sub { $destdir = $_[1];  },
+  'verbose|V'     => sub { $verbose = 1;      },
+  'blogdir|b=s'   => sub { $blogdir = $_[1];  },
 );
 
 # Check missing options:
 exit_with_error("Wrong number of options.", 22) if (scalar(@ARGV) != 1);
 
 # Check the repository is present (however naive this method is):
-exit_with_error("Not a Blaze repository! Try `blaze-init' first.", 1)
-  unless (-d catdir($destdir, '.blaze'));
+exit_with_error("Not a BlazeBlogger repository! Try `blaze-init' first.",1)
+  unless (-d catdir($blogdir, '.blaze'));
 
 # Prepare the file names:
-my $head = catfile($destdir, '.blaze', "${type}s", 'head', $ARGV[0]);
-my $body = catfile($destdir, '.blaze', "${type}s", 'body', $ARGV[0]);
-my $log  = catfile($destdir, '.blaze', 'log');
-
-# Check whether the record exists:
-unless (-e $head) {
-  # Report failure:
-  print "There is no $type with ID $ARGV[0].\n" if $verbose;
-
-  # Return failure:
-  exit 1; 
-}
+my $head = catfile($blogdir, '.blaze', "${type}s", 'head', $ARGV[0]);
+my $body = catfile($blogdir, '.blaze', "${type}s", 'body', $ARGV[0]);
 
 # Remove the files:
-unlink($head) || exit_with_error("Unable to delete `$head'.", 13);
-unlink($body) || exit_with_error("Unable to delete `$body'.", 13);
+unlink($head) and unlink($body)
+  or exit_with_error("Unable to delete the record with ID $ARGV[0].", 13);
 
 # Log the record deletion:
-add_to_log($log, "Removed the $type with ID $ARGV[0].");
+add_to_log("Removed the $type with ID $ARGV[0].")
+  or exit_with_error("Unable to log the event.");
 
 # Report success:
 print "The $type has been successfully removed.\n" if $verbose;
@@ -151,27 +154,28 @@ __END__
 
 =head1 NAME
 
-blaze-remove - remove a blog post or a page from the Blaze repozitory
+blaze-remove - remove a blog post or a page from the BlazeBlogger
+repository
 
 =head1 SYNOPSIS
 
-B<blaze-remove> [B<-pqP>] [B<-d> I<directory>] I<id>
+B<blaze-remove> [B<-pqPV>] [B<-b> I<directory>] I<id>
 
 B<blaze-remove> B<-h> | B<-v>
 
 =head1 DESCRIPTION
 
 B<blaze-remove> deletes the blog post or static page with given I<id> from
-the Blaze repository.
+the BlazeBlogger repository.
 
 =head1 OPTIONS
 
 =over
 
-=item B<-d>, B<--destdir> I<directory>
+=item B<-b>, B<--blogdir> I<directory>
 
-Use selected destination I<directory> instead of the default current
-working one.
+Specify the I<directory> where the BlazeBlogger repository is placed. The
+default option is the current working directory.
 
 =item B<-p>, B<--page>
 
@@ -184,6 +188,10 @@ Remove the blog post; this is the default option.
 =item B<-q>, B<--quiet>
 
 Avoid displaying messages that are not necessary.
+
+=item B<-V>, B<--verbose>
+
+Display all messages. This is the default option.
 
 =item B<-h>, B<--help>
 
@@ -209,7 +217,7 @@ version published by the Free Software Foundation; with no Invariant
 Sections, no Front-Cover Texts, and no Back-Cover Texts.
 
 A copy of the license is included as a file called FDL in the main
-directory of the blaze source package.
+directory of the BlazeBlogger source package.
 
 =head1 COPYRIGHT
 
