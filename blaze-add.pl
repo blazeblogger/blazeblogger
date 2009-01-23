9f8bd76b143097bd7a2dde7b06f5fe91663f0113
Jaromir Hradilek <jhradilek@gmail.com>
2009-01-23 23:17:25 +0100

    Partly rewrited the post/page adding utility.
    Among various cosmetic changes, the most important are:
    
      * the much complete set of command-line options with more obvious names
        and description;
      * addition of the url option to be used for pretty url later;
      * addition of save_record() function to simplify matters creating another
        abstraction layer;
      * making the functions (almost) independent on global variables, because
        it is not only ugly, but potentialy unpredictable; there are still
        $blogdir and $verbose, but those make sense (at least to me) and does
        not produce unexpected results;
      * making functions return values instead of just exiting with error code,
        at least at places where this behaviour is not desirable.

--- blaze-add.pl
+++ blaze-add.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# blaze-add, add a blog post or a page to the Blaze repository
+# blaze-add, add a blog post or a page to the BlazeBlogger repository
 # Copyright (C) 2008, 2009 Jaromir Hradilek
 
 # This program is  free software:  you can redistribute it and/or modify it
@@ -29,14 +29,15 @@ use constant NAME    => basename($0, '.pl');        # Script name.
 use constant VERSION => '0.0.1';                    # Script version.
 
 # General script settings:
-our $type    = 'post';                              # Type: post or page.
-our $destdir = '.';                                 # Destination folder.
+our $blogdir = '.';                                 # Repository location.
 our $verbose = 1;                                   # Verbosity level.
+
+# Command-line options:
+my  $type = 'post';                                 # Type: post or page.
 
 # Set up the __WARN__ signal handler:
 $SIG{__WARN__} = sub {
-  my $message  = shift;
-  print STDERR NAME . ": $message";
+  print STDERR NAME . ": " . (shift);
 };
 
 # Display given message and terminate the script:
@@ -52,23 +53,30 @@ sub exit_with_error {
 sub display_help {
   my $NAME = NAME;
 
+  # Print the message to the STDOUT:
   print << "END_HELP";
-Usage: $NAME [-pqP] [-d directory] [file]
+Usage: $NAME [-pqPV] [-b directory] [file...]
        $NAME -h | -v
 
-  -d, --destdir directory     specify the destination directory
+  -b, --blogdir directory     specify the directory where the BlazeBlogger
+                              repository is placed
   -p, --page                  add the static page instead of the post
   -P, --post                  add the blog post; the default option
   -q, --quiet                 avoid displaying unnecessary messages
+  -V, --verbose               display all messages; the default option
   -h, --help                  display this help and exit
   -v, --version               display version information and exit
 END_HELP
+
+  # Return success:
+  return 1;
 }
 
 # Display version information:
 sub display_version {
   my ($NAME, $VERSION) = (NAME, VERSION);
 
+  # Print the message to the STDOUT:
   print << "END_VERSION";
 $NAME $VERSION
 
@@ -78,6 +86,9 @@ distributed in the hope  that it will be useful,  but WITHOUT ANY WARRANTY;
 without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PAR-
 TICULAR PURPOSE.
 END_VERSION
+
+  # Return success:
+  return 1;
 }
 
 # Write string to the given file:
@@ -85,39 +96,87 @@ sub write_to_file {
   my $file = shift || die "Missing argument";
   my $text = shift || '';
 
-  # Try to open the file for writing:
-  if (open(FOUT, ">$file")) {
-    # Write given string:
-    print FOUT $text;
+  # Open the file for writing:
+  open(FILE, ">$file") or return 0;
 
-    # Close the file:
-    close(FOUT);
+  # Write given string to the file::
+  print FILE $text;
+
+  # Close the file:
+  close(FILE);
+
+  # Return success:
+  return 1;
+}
+
+# Create a record from the single file:
+sub save_record {
+  my $file = shift || die "Missing argument";
+  my $id   = shift || die "Missing argument";
+  my $type = shift || 'post';
+  my $data = {};
+  my $line = '';
+
+  # Prepare the record file names:
+  my $head = catfile($blogdir, '.blaze', "${type}s", 'head', $id);
+  my $body = catfile($blogdir, '.blaze', "${type}s", 'body', $id);
+
+  # Open the file for reading:
+  open(FILE, "$file") or return 0;
+
+  # Parse the file header:
+  while ($line = <FILE>) {
+    # Header ends with the first line not beginning with `#':
+    last unless $line =~ /^#/;
+
+    # Collect the data for the record header:
+    if ($line =~ /(title|author|date|tags|url):\s*(\S.*)$/) {
+      $data->{header}->{$1} = $2;
+    }
   }
-  else {
-    # Report failure and exit:
-    exit_with_error("Unable to write to `$file'.", 13);
+
+  # TODO: Add data checks.
+
+  # Write the record header:
+  WriteINI($head, $data) or return 0;
+
+  # Open the record body for writing:
+  open(BODY, ">$body") or return 0;
+
+  # Write the last read line to the body record:
+  print BODY $line if $line;
+
+  # Add the rest of the file content to the body record:
+  while ($line = <FILE>) {
+    print BODY $line;
   }
+
+  # Close previously opened files:
+  close(BODY);
+  close(FILE);
+
+  # Return success:
+  return 1;
 }
 
 # Add given string to the log file
 sub add_to_log {
-  my $file = shift || die "Missing argument";
-  my $text = shift || '';
+  my $text = shift || 'Something miraculous has just happened!';
+  my $file = catfile($blogdir, '.blaze', 'log');
 
-  # Try to open the log file:
-  if (open(LOG, ">>$file")) {
-    # Write to the log file: 
-    print LOG "Date: ".strftime("%a %b %e %H:%M:%S %Y", localtime)."\n\n";
-    print LOG wrap('    ', '    ', $text);
-    print LOG "\n\n";
+  # Open the log file for appending:
+  open(LOG, ">>$file") or return 0;
 
-    # Close the file:
-    close(LOG);
-  }
-  else {
-    # Report failure and exit:
-    exit_with_error("Unable to write to `$file'.", 13);
-  }
+  # Write to the log file: 
+  print LOG "Date: ".strftime("%a %b %e %H:%M:%S %Y", localtime)."\n\n";
+  print LOG wrap('    ', '    ', $text);
+  print LOG "\n\n";
+
+  # Close the file:
+  close(LOG);
+
+  # Return success:
+  return 1;
 }
 
 # Translate given date to YYYY-MM-DD string:
@@ -128,27 +187,23 @@ sub date_to_string {
 
 # Return the first unused ID:
 sub choose_id {
-  my $head   = shift || die "Missing argument";
-  my @used   = ();
+  my $type   = shift || 'post';
+  my $head   = catdir($blogdir, '.blaze', "${type}s", 'head');
   my $chosen = 1;
 
-  # Open the heads directory:
-  if (opendir(HEADS, $head)) {
-    # Build a list of used IDs:
-    @used = grep {! /^\.\.?$/ } readdir(HEADS);
+  # Open the headers directory:
+  opendir(HEADS, $head) or return 0;
 
-    # Find a first unused ID:
-    foreach my $id (sort {$a <=> $b} @used) {
-      $chosen++ if ($chosen == $id);
-    }
+  # Build a list of used IDs:
+  my @used = grep {! /^\.\.?$/ } readdir(HEADS);
 
-    # Close the directory:
-    closedir(HEADS);
+  # Find the first unused ID:
+  foreach my $id (sort {$a <=> $b} @used) {
+    $chosen++ if ($chosen == $id);
   }
-  else {
-    # Report failure:
-    exit_with_error("Unable to access `$head' directory.", 13);
-  }
+
+  # Close the directory:
+  closedir(HEADS);
 
   # Return the result:
   return $chosen;
@@ -164,30 +219,25 @@ GetOptions(
   'page|p'        => sub { $type    = 'page'; },
   'post|P'        => sub { $type    = 'post'; },
   'quiet|q'       => sub { $verbose = 0;      },
-  'destdir|d=s'   => sub { $destdir = $_[1];  },
+  'verbose|V'     => sub { $verbose = 1;      },
+  'blogdir|b=s'   => sub { $blogdir = $_[1];  },
 );
 
-# Check superfluous options:
-exit_with_error("Wrong number of options.", 22) if (scalar(@ARGV) > 1);
-
 # Check the repository is present (however naive this method is):
-exit_with_error("Not a Blaze repository! Try `blaze-init' first.", 1)
-  unless (-d catdir($destdir, '.blaze'));
+exit_with_error("Not a BlazeBlogger repository! Try `blaze-init' first.",1)
+  unless (-d catdir($blogdir, '.blaze'));
 
-# If provided, use existing file:
-my $file = $ARGV[0] || '';
-
-# Unless the file is supplied, create a new one:
-unless ($file) {
-  $file = catfile($destdir, '.blaze', 'temp');
+# Check whether the file is supplied:
+if (scalar(@ARGV) == 0) {
+  my $file = catfile($blogdir, '.blaze', 'temp');
 
   # Read the configuration file:
-  my $temp = catfile($destdir, '.blaze', 'config');
+  my $temp = catfile($blogdir, '.blaze', 'config');
   my $conf = ReadINI($temp)
              or exit_with_error("Unable to read `$temp'.", 13);
 
   # Prepare the data for the temporary file header:
-  my $name = $conf->{user}->{name} || 'Replace this with your name';
+  my $name = $conf->{user}->{name} || 'admin';
   my $date = date_to_string(time);
 
   # Decide which editor to use:
@@ -197,13 +247,16 @@ unless ($file) {
   write_to_file($file, << "END_TEMP");
 # This and following lines beginning with  `#' are the $type header.  Please
 # take your time and replace these options with desired values. Just remem-
-# ber that the date has to be in an YYYY-MM-DD form and that  the tags is a
-# comma separated list of categories the post (pages ignore these)  belong.
+# ber that the date has to be in an  YYYY-MM-DD  form,  the tags is a comma
+# separated list of categories the post (pages ignore these) belong and the
+# URL, if provided, should consist of alphaanumeric characters, hyphens and
+# underscores only.
 #
 #   title:
 #   author: $name
 #   date:   $date
 #   tags:
+#   url:
 #
 # The header ends here. The rest is the content of your $type.
 
@@ -211,51 +264,45 @@ END_TEMP
 
   # Open the temporary file in the external editor:
   system($edit, $file) == 0 or exit_with_error("Unable to run `$edit'.",1);
-}
 
-# Open the file for reading:
-if (open(TEMP, "$file")) {
-  my $header = {};
-  my $line   = '';
+  # Get the first unused ID:
+  my $id = choose_id($type);
 
-  # Choose the first available ID:
-  my $id   = choose_id(catdir($destdir, '.blaze', "${type}s", 'head'));
-
-  # Prepare the file names:
-  my $head = catfile($destdir, '.blaze', "${type}s", 'head', $id);
-  my $body = catfile($destdir, '.blaze', "${type}s", 'body', $id);
-  my $log  = catfile($destdir, '.blaze', 'log');
-
-  # Process the header:
-  while ($line = <TEMP>) {
-    # Header ends with first line not beginning with `#':
-    last unless $line =~ /^#/;
-
-    # Parse header data:
-    if ($line =~ /(title|author|date|tags):\s*(\S.*)$/) {
-      $header->{header}->{$1} = $2;
-    }
-  }
-
-  # Write the header:
-  WriteINI($head, $header)
-    or exit_with_error("Unable to write to `$head'.", 13);
-
-  # Write the body:
-  write_to_file($body, $line . do { local $/; <TEMP> });
-
-  # Close the temporary file:
-  close(TEMP);
+  # Save the record:
+  save_record($file, $id, $type)
+    or exit_with_error("Unable to write the record.", 13);
 
   # Log the record addition:
-  add_to_log($log, "Added the $type with ID $id.");
+  add_to_log("Added the $type with ID $id.")
+    or exit_with_error("Unable to log the event.");
 
   # Report success:
   print "The $type has been successfully added with ID $id.\n" if $verbose;
 }
 else {
-  # Report failure:
-  exit_with_error("Unable to read `$file'.", 13);
+  my @added = ();
+
+  # Process each file:
+  foreach my $file (@ARGV) {
+    # Get the first unused ID:
+    my $id = choose_id($type);
+
+    # Save the record:
+    save_record($file, $id, $type)
+      and push(@added, $id)
+      or print STDERR NAME . ": Unable to add $file.\n" if $verbose;
+  }
+
+  # Prepare the list of successfully added IDs:
+  my $list = join(', ', sort(@added));
+
+  # Log the record addition:
+  add_to_log("Added the $type with ID $list.")
+    or exit_with_error("Unable to log the event.") if $list;
+
+  # Report success:
+  print "Successfully added the $type with ID $list.\n"
+    if ($verbose && $list);
 }
 
 # Return success:
@@ -265,28 +312,28 @@ __END__
 
 =head1 NAME
 
-blaze-add - add a blog post or a page to the Blaze repository
+blaze-add - add a blog post or a page to the BlazeBlogger repository
 
 =head1 SYNOPSIS
 
-B<blaze-add> [B<-pqP>] [B<-d> I<directory>] [I<file>]
+B<blaze-add> [B<-pqPV>] [B<-b> I<directory>] [I<file>...]
 
 B<blaze-add> B<-h> | B<-v>
 
 =head1 DESCRIPTION
 
-B<blaze-add> adds new blog posts or static pages to the Blaze repository.
-If supplied, it tries to read data from the existing I<file>, otherwise an
-external editor is opened to let you create a new content.
+B<blaze-add> adds new blog posts or static pages to the BlazeBlogger
+repository. If supplied, it tries to read data from the existing I<file>s,
+otherwise an external editor is opened to let you create a new content.
 
 =head1 OPTIONS
 
 =over
 
-=item B<-d>, B<--destdir> I<directory>
+=item B<-b>, B<--blogdir> I<directory>
 
-Use selected destination I<directory> instead of the default current
-working one.
+Specify the I<directory> where the BlazeBlogger repository is placed. The
+default option is the current working directory.
 
 =item B<-p>, B<--page>
 
@@ -299,6 +346,10 @@ Add the blog post; this is the default option.
 =item B<-q>, B<--quiet>
 
 Avoid displaying messages that are not necessary.
+
+=item B<-V>, B<--verbose>
+
+Display all messages. This is the default option.
 
 =item B<-h>, B<--help>
 
@@ -316,9 +367,9 @@ Display version information and exit.
 
 =item B<EDITOR>
 
-Unless the Blaze specific option I<core.editor> is set, blaze-edit tries to
-use system wide settings to decide which editor to run. If neither of these
-options are supplied, the B<vi> is used instead as a considerably
+Unless the BlazeBlogger specific option I<core.editor> is set, blaze-edit
+tries to use system wide settings to decide which editor to run. If neither
+of these options are supplied, the B<vi> is used instead as a considerably
 reasonable choice.
 
 =back
@@ -329,7 +380,7 @@ reasonable choice.
 
 =item I<.blaze/config>
 
-Blaze configuration file.
+BlazeBlogger configuration file.
 
 =back
 
@@ -347,7 +398,7 @@ version published by the Free Software Foundation; with no Invariant
 Sections, no Front-Cover Texts, and no Back-Cover Texts.
 
 A copy of the license is included as a file called FDL in the main
-directory of the blaze source package.
+directory of the BlazeBlogger source package.
 
 =head1 COPYRIGHT
 
