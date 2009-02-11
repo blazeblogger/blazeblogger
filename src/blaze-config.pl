ab0cd934b44bbad500c4cc4c8d4cc7abbae2dd32
Jaromir Hradilek <jhradilek@gmail.com>
2009-02-11 01:47:13 +0100

    Increased version.

--- src/blaze-config.pl
+++ src/blaze-config.pl
@@ -24,7 +24,7 @@ use Getopt::Long;
 
 # General script information:
 use constant NAME    => basename($0, '.pl');        # Script name.
-use constant VERSION => '0.7.0';                    # Script version.
+use constant VERSION => '0.7.1';                    # Script version.
 
 # General script settings:
 our $blogdir = '.';                                 # Repository location.
