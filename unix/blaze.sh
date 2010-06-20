c454a1f4c07be9a8f88f7889d6630f0c1a37f43d
Jaromir Hradilek <jhradilek@gmail.com>
2010-06-21 00:01:40 +0200

    Commented out the blaze-extras related parts.

--- unix/blaze.sh
+++ unix/blaze.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # blaze, a command wrapper for BlazeBlogger
-# Copyright (C) 2009 Jaromir Hradilek
+# Copyright (C) 2009, 2010 Jaromir Hradilek
 
 # This program is  free software:  you can redistribute it and/or modify it
 # under  the terms  of the  GNU General Public License  as published by the
@@ -31,20 +31,20 @@ case "$COMMAND" in
     # Run selected core utility:
     exec blaze-$COMMAND "$@"
     ;;
-  "submit" | "gallery")
-    # Make sure that blazeblogger-extras are installed:
-    if ! which blaze-$COMMAND > /dev/null 2>&1; then
-      # Report failure:
-      echo "$NAME: Unable to find \`blaze-$COMMAND'." >&2
-      echo "Are blazeblogger-extras installed?" >&2
-
-      # Return failure:
-      exit 127
-    fi
-
-    # Run selected extra utility:
-    exec blaze-$COMMAND "$@"
-    ;;
+# "submit" | "gallery")
+#   # Make sure that blazeblogger-extras are installed:
+#   if ! which blaze-$COMMAND > /dev/null 2>&1; then
+#     # Report failure:
+#     echo "$NAME: Unable to find \`blaze-$COMMAND'." >&2
+#     echo "Are blazeblogger-extras installed?" >&2
+#
+#     # Return failure:
+#     exit 127
+#   fi
+#
+#   # Run selected extra utility:
+#   exec blaze-$COMMAND "$@"
+#   ;;
   "-h" | "--help" | "help")
     # Get user supplied command (if any):
     COMMAND=$1
@@ -55,20 +55,20 @@ case "$COMMAND" in
         # Display core utility usage information:
         exec blaze-$COMMAND --help
         ;;
-      "submit" | "gallery")
-        # Make sure that blazeblogger-extras are installed:
-        if ! which blaze-$COMMAND > /dev/null 2>&1; then
-          # Report failure:
-          echo "$NAME: Unable to find \`blaze-$COMMAND'." >&2
-          echo "Are blazeblogger-extras installed?" >&2
-
-          # Return failure:
-          exit 127
-        fi
-
-        # Display extra utility usage information:
-        exec blaze-$COMMAND --help
-        ;;
+#     "submit" | "gallery")
+#       # Make sure that blazeblogger-extras are installed:
+#       if ! which blaze-$COMMAND > /dev/null 2>&1; then
+#         # Report failure:
+#         echo "$NAME: Unable to find \`blaze-$COMMAND'." >&2
+#         echo "Are blazeblogger-extras installed?" >&2
+#
+#         # Return failure:
+#         exit 127
+#       fi
+#
+#       # Display extra utility usage information:
+#       exec blaze-$COMMAND --help
+#       ;;
       *)
         # Display list of available commands:
         echo "Usage: $NAME COMMAND [OPTION...]"
@@ -83,10 +83,10 @@ case "$COMMAND" in
         echo "  make     Generate static content from the BlazeBlogger repository."
         echo "  log      Display the BlazeBlogger repository log."
         echo
-        echo "Extra commands:"
-        echo "  submit   Upload the static content to the remote server."
-        echo "  gallery  Create a simple image gallery."
-        echo
+#       echo "Extra commands:"
+#       echo "  submit   Upload the static content to the remote server."
+#       echo "  gallery  Create a simple image gallery."
+#       echo
         echo "Additional commands:"
         echo "  help [COMMAND]  Display usage information (on specified command)."
         echo "  man [COMMAND]   Display manual page (on specified command)."
@@ -109,10 +109,10 @@ case "$COMMAND" in
         # Display core utility usage information:
         exec man blaze-$COMMAND
         ;;
-      "submit" | "gallery")
-        # Display extra utility usage information:
-        exec man blaze-$COMMAND
-        ;;
+#     "submit" | "gallery")
+#       # Display extra utility usage information:
+#       exec man blaze-$COMMAND
+#       ;;
       *)
         # Display general manual page:
         exec man blazeblogger
@@ -123,7 +123,7 @@ case "$COMMAND" in
     # Display version information:
     echo "BlazeBlogger $VERSION"
     echo
-    echo "Copyright (C) 2008, 2009 Jaromir Hradilek"
+    echo "Copyright (C) 2008, 2009, 2010 Jaromir Hradilek"
     echo "This program is free software; see the source for copying conditions. It is"
     echo "distributed in the hope  that it will be useful,  but WITHOUT ANY WARRANTY;"
     echo "without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PAR-"
